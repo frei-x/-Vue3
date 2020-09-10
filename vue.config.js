68cf0dd8a881cc850842980a75169716dcecc664
衣沾不足惜 <javascript.h@qq.com>
2020-09-10 19:02:47 +0800

    本地 link dev模式 排除 eslint 检测软连接
    Signed-off-by: 衣沾不足惜 <javascript.h@qq.com>

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   configureWebpack: {
-    devtool: 'source-map'
+    devtool: 'source-map',
+  },
+  //本地 link dev模式 排除 eslint 检测软连接
+  chainWebpack: (config) => {
+    config.module.rule('eslint').exclude.add(/vue-dust/).end();
   },
   outputDir: 'dist',
   assetsDir: 'static',
-  publicPath: './'
-}
+  publicPath: './',
+};
